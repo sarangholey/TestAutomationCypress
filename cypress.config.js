0ea6a9bc4374516fdc119efd6ec2a3ef3afeea67
sarangholey <sarangholey@gmail.com>
2024-10-21 00:35:05 +0530

    Ened To End Scenario

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // Global Element timeout
+  defaultCommandTimeout: 6000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/intigration/examples/Test9FrameworkPOM.js
+++ cypress/intigration/examples/Test9FrameworkPOM.js
@@ -28,19 +28,48 @@ describe('PageObjectModel', function() {
     })
 
     it('PageObjectModel TC2', function() {
-
         cy.visit('https://rahulshettyacademy.com/angularpractice/');
         homePage.clickOnShop().click();
         cy.SelectProduct('Nokia Edge');
         //cy.pause();
         cy.SelectProduct('Blackberry');
         shoppingPage.checkoutButton().click();
+
+        // All Products Individual total validation with Final Checkout total value
+        var sumOfAllProducts = 0;
+        var totalCartTotal = 0;
+        cy.get('tr td:nth-child(4) > strong').each(($el, index, $list) => {
+
+            const amountText = $el.text();
+            var amountValue = amountText.split(" ");
+            amountValue = amountValue[1].trim();
+            sumOfAllProducts = Number(sumOfAllProducts) + Number(amountValue);
+        }).then(function() {
+            cy.log(sumOfAllProducts)
+        })
+        
+        cy.get('h3 > strong').then(function(elemnt) {
+            const totalCartValue = elemnt.text();
+            var totalCartTotalArr = totalCartValue.split(" ");
+            // cy.log ("Test" );
+            // cy.log (Number(totalCartTotalArr[1]))
+            totalCartTotal = Number(totalCartTotalArr[1]);
+            expect(totalCartTotal).to.be.equal(sumOfAllProducts);
+        })
+
+
         checkoutPage.checkoutPageButton().click();
         purchasePage.typeCountryName().type(this.data.CountryName);
-        cy.wait(3000);
+        Cypress.config('defaultCommandTimeout', 8000)
+        //cy.wait(3000);
         purchasePage.suggestedCountryName().click();
         purchasePage.purchaseButtton().click();
         purchasePage.purchaseSuccessMsg().should('contain.text', "Success! Thank you! Your order will be delivered in next few weeks :-).");
+        //or
+        purchasePage.purchaseSuccessMsg().then(function(elemnt) {
+            const actualText = elemnt.text();
+            expect(actualText.includes("Success")).to.be.true
+        })
     })
 
 
